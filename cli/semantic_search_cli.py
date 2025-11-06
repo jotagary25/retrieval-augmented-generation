e87e80d331aa84c204c191a80856c64cb132bd1b
Gary Fernandez <jotagary25@protonmail.com>
2025-11-05 21:09:39 -0400

    :construction: semantic search structure done

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,11 +2,20 @@
 
 import argparse
 
+from lib.semantic_search import verify_model
+
+
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    verify_parser = subparsers.add_parser("verify", help="Verify the model of the semantic search")
+
     args = parser.parse_args()
 
     match args.command:
+        case "verify":
+            verify_model()
         case _:
             parser.print_help()
 
